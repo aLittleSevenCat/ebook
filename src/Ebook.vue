247d2f85bd1183acab521cde9196814c27620a10
aLittleCat <1375116217@qq.com>
2018-11-26 17:27:38 +0800

    first commit

---
+++ src/Ebook.vue
@@ -0,0 +1,25 @@
+<template>
+ <div>
+     aaa
+ </div>
+</template>
+
+<script>
+import Epub from 'epubjs'
+const DOWLOAD_URL = '/static/2018_Book_AgileProcessesInSoftwareEngine'
+export default {
+  methods: {
+    showEpub () {
+      this.book = new Epub(DOWLOAD_URL)
+      console.log(this.book)
+    }
+  },
+  mounted () {
+
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
